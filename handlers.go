4938e5bb939ead847600638405f2168b43705286
Vibin <vibin.manavalan@gmail.com>
2023-11-28 19:50:39 +0100

    add routes

--- handlers.go
+++ handlers.go
@@ -5,13 +5,14 @@ import (
 	"go_net_watcher/internal/netwatcher"
 )
 
-var test *netwatcher.AppConfig
-
-func NewWebTest() *netwatcher.AppConfig {
-	return test
+func NewWebTest() map[string]netwatcher.NetDevices {
+	var test *netwatcher.AppConfig
+	return test.FinalMap
 }
 
 func home(c *fiber.Ctx) error {
-	//gg := NewWebTest()
-	return c.Render("index", nil)
+	gg := NewWebTest()
+	FinalSerializer := make(map[string]netwatcher.NetDevices)
+	FinalSerializer = gg
+	return c.Render("index", FinalSerializer)
 }
